35f430b419a8108cbca9138687ecccd4ba8cbef2
tuyihero <tuyihero@163.com>
2017-05-27 17:57:06 +0800

    scenes navmesh

---
+++ Script/Editor/CommonTool.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class CommonTool : Editor
+{
+
+    [MenuItem("TyTools/Map/DisableMapCollider")]
+    public static void DisableMapCollider()
+    {
+        var selects = Selection.GetFiltered(typeof(GameObject), SelectionMode.TopLevel);
+        foreach (var selectGO in selects)
+        {
+            DisableCollider(selectGO as GameObject);
+        }
+    }
+
+    #region particleTime
+
+    private static void DisableCollider(GameObject particleObj)
+    {
+        var colliders = particleObj.GetComponentsInChildren<Collider>();
+        foreach (var collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    #endregion
+    
+}
