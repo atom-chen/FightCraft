cbb6d794ce9e009b325d480cb947ae8157771dbf
tuyihero <tuyiherizon@163.com>
2017-11-27 08:57:52 +0800

    equip impact

--- Script/Fight/RoleAttr/RoleAttrImpactPassiveHitCircle.cs
+++ Script/Fight/RoleAttr/RoleAttrImpactPassiveHitCircle.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class RoleAttrImpactPassiveHitCircle : RoleAttrImpactPassive
 {
+    public override void InitImpact(string skillInput, List<int> args)
+    {
+        base.InitImpact(skillInput, args);
+
+        var legendaryEquip = Tables.TableReader.LegendaryEquip.GetRecord(args[0].ToString());
+        _Range = GameDataValue.ConfigIntToFloat(legendaryEquip.ImpactValues[0]) + GameDataValue.ConfigIntToFloat(legendaryEquip.ImpactValueIncs[0] * args[1]);
+
+
+        _DamageValue = GameDataValue.ConfigIntToFloat(legendaryEquip.ImpactValues[1]) + GameDataValue.ConfigIntToFloat(legendaryEquip.ImpactValueIncs[1] * args[1]);
+    }
 
     public override void ModifySkillAfterInit(MotionManager roleMotion)
     {
@@ -11,9 +21,19 @@ public class RoleAttrImpactPassiveHitCircle : RoleAttrImpactPassive
             return;
 
         var buffGO = ResourceManager.Instance.GetInstanceGameObject("Bullet\\Passive\\" + _ImpactName);
-        var buffs = buffGO.GetComponents<ImpactBuff>();
+        buffGO.transform.SetParent(roleMotion.BuffBindPos.transform);
+        var buffs = buffGO.GetComponents<ImpactBuffIntervalRangeSub>();
         foreach (var buff in buffs)
         {
+            var subBuffs2 = buffGO.GetComponentsInChildren<ImpactDamage>();
+            foreach (var subBuff in subBuffs2)
+            {
+                if (subBuff.gameObject == buffGO)
+                    continue;
+                subBuff._DamageRate = _DamageValue;
+            }
+
+            buff._Range = _Range;
             buff.ActImpact(roleMotion, roleMotion);
         }
     }
@@ -21,6 +41,8 @@ public class RoleAttrImpactPassiveHitCircle : RoleAttrImpactPassive
 
     #region 
 
-    
+    private float _Range;
+    private float _DamageValue;
+
     #endregion
 }
